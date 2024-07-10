ef58df797da1835b34fe681fb9df5da0f1f12abf
WagnerMinelo <151886224+WagnerMinelo@users.noreply.github.com>
2024-07-10 11:51:51 -0300

    Add files via upload

---
+++ aula31py.py
@@ -0,0 +1,43 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+"""
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
+
+# condicao = False
+
+# if condicao:
+#   print('Faça algo')
+# else:
+#      print('Não faça algo')
+
+"""
+Flag (Bandeira) - Marcar um local
+None = não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id - identidade
+"""
+
+condicao = False
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+
+if passou_no_if is not None:
+    print('Passou no if')
+
+
+# print(passou_no_if, passou_no_if is None)
+# print(passou_no_if, passou_no_if is not None)
