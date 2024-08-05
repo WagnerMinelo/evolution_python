28249ed9fa6aa80f28dcf75b4ebd78d28137e570
WagnerMinelo <151886224+WagnerMinelo@users.noreply.github.com>
2024-08-05 18:32:00 -0300

    Add files via upload

---
+++ aula48py.py
@@ -0,0 +1,73 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - indices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+#.........+01234
+#.........-54321
+# string = 'ABCDE' # 5 caracteres (len)
+# print(bool(lista)) # falsy
+# print(lista, type(lista))
+
+#........0.....1............2.........3....4
+#........-5....-4...........-3.......-2...-1
+# lista = [123, True, 'Wagner Minelo', 1.2, []]
+# lista[-3] = 'Maria'
+# print(lista)
+# print(lista[2].upper(), type(lista[2]))
+""""""""""""
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - indices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+Create Read Update  Delete
+Criar, ler, alterar, apagar = lista [i] (CRUD)
+"""
+# lista = [10,20,30,40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+# lista.append(50)
+# lista.pop()
+# lista.append(60)
+# lista.append(70)
+# ultimo_valor = lista.pop(3)
+# print(lista, 'Removido', ultimo_valor)
+""""""""""""
+""" Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - indices e fatiamento
+Métodos úteis: 
+= append, insert, pop, del, clear, extend, +
+= Create Read Update  Delete
+=Criar, ler, alterar, apagar = lista [i] (CRUD)
+append = Adiciona um item ao final
+insert = Adiciona um item no indice escolhido
+pop = Remove do final ou do indice escolhido
+del = apaga um indice
+clear = limpa a lista
+extend = estende a lista
++ - concatena lista
+"""
+#........0..1..2...3
+# lista = [10, 20, 30, 40]
+# lista.append('Wagner')
+# nome = lista.pop()
+# lista.append(1234)
+# del lista[-1]
+# lista.clear()
+# lista.insert(100, 5)
+# print(lista[4])
+""""""
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_a)
