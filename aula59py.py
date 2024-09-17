144ad6edf7b17f7e12bc8885ef80362ad3fd9ecb
WagnerMinelo <151886224+WagnerMinelo@users.noreply.github.com>
2024-09-17 10:31:36 -0300

    Add files via upload

---
+++ aula59py.py
@@ -0,0 +1,27 @@
+# Desempacotamento em chamadas
+# de métodos e funções
+string = 'ABCD'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+salas = [
+    #         1
+    ['Maria', 'Helena', ], # 0
+    # 0
+    ['Elaine',], # 1
+    # 0       1     2
+    ['Luiz', 'João', 'Eduarda', ], # 2
+
+]
+
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# for nome in lista:
+   # print(nome, end=' ', sep=' ')
+
+# print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+print(*salas, sep='\n')
---
+++ aula60py.py
@@ -0,0 +1,13 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+"""
+# print('Valor' if True else 'Outro valor')
+# condicao = 10 == 10
+# variavel = 'Valor' if condicao else 'Outro valor'
+# print(variavel)
+# digito = 10 # > 9 = 0
+# novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+print('Valor' if False else 'Outro valor' if False else 'Fim')
