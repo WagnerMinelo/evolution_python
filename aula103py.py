0d429335f37a79783c1d6a9f39ebf2483c02f9ce
WagnerMinelo <151886224+WagnerMinelo@users.noreply.github.com>
2024-12-02 15:57:59 -0300

    Add files via upload

---
+++ aula103py.py
@@ -0,0 +1,29 @@
+# Funções decoradoras e decoradores
+# Decorar = Adicionar / Remover / Restringir / Alterar
+# Funções decoradoras são funções que decoram outras funções
+# Decoradores são usados para fazer o Python
+# usar as funções decoradoras em outras funções
+
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        print('vou te decorar')
+        for arg in args:
+            e_string(arg)
+        resultado = func(*args, **kwargs)
+        print(f'o seu resultado foi {resultado}. ')
+        print('ok, você foi decorado')
+        return resultado
+    return interna
+
+def inverte_string(string):
+    return string[::-1]
+
+def e_string(param):
+    if not isinstance(param, str):
+        raise TypeError('param deve ser uma string')
+
+
+invert_string_checando_parametro = criar_funcao(inverte_string)
+invertida = invert_string_checando_parametro('123')
+print(invertida)
+
